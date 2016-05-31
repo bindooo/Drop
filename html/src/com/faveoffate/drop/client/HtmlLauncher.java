d7648bc0418b6fdf0200ff545197930a311150da
bindooo <bindicsgeri@gmail.com>
2016-05-31 15:55:32 +0200

    Drop extended simple game

---
+++ html/src/com/faveoffate/drop/client/HtmlLauncher.java
@@ -0,0 +1,24 @@
+package com.faveoffate.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.faveoffate.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Drop();
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}

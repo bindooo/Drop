bddf959e05e64617abeef58503a026aa43048fb6
bindooo <bindicsgeri@gmail.com>
2016-06-09 10:37:53 +0200

    TouchTest added

--- core/src/com/faveoffate/drop/GameScreen.java
+++ core/src/com/faveoffate/drop/GameScreen.java
@@ -101,17 +101,35 @@ public class GameScreen implements Screen {
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
             bucket.x = touchPos.x - 64 / 2;
+
+        ///////////////////////////////////////////////////////////////////
+
+            bucket.y = touchPos.y - 64 / 2;
         }
         if (Gdx.input.isKeyPressed(Keys.LEFT))
-            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+            bucket.x -= 300 * Gdx.graphics.getDeltaTime();
         if (Gdx.input.isKeyPressed(Keys.RIGHT))
-            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+            bucket.x += 300 * Gdx.graphics.getDeltaTime();
+
+        ///////////////////////////////////////////////////////////////////
+
+        if (Gdx.input.isKeyPressed(Keys.DOWN))
+            bucket.y -= 300 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Keys.UP))
+            bucket.y += 300 * Gdx.graphics.getDeltaTime();
 
         // make sure the bucket stays within the screen bounds
         if (bucket.x < 0)
             bucket.x = 0;
         if (bucket.x > 800 - 64)
             bucket.x = 800 - 64;
+
+        ///////////////////////////////////////////////////////////////////
+
+        if (bucket.y < 0)
+            bucket.y = 0;
+        if (bucket.y > 480 - 64)
+            bucket.y = 480 - 64;
 
         // check if we need to create a new raindrop
         if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
--- desktop/src/com/faveoffate/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/faveoffate/drop/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.faveoffate.drop.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.faveoffate.drop.Drop;
+import com.faveoffate.drop.SimplerTouchTest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -11,5 +12,14 @@ public class DesktopLauncher {
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
+
 	}
+	/*
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.useHDPI = true;
+		new LwjglApplication(new SimplerTouchTest(), config);
+	}*/
 }
